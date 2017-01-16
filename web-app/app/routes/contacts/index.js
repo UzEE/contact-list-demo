8f78e22390a4bd2a3ee7d4a761ae6a38811caaed
Uzair Sajid <uzee@uzeeinc.net>
2017-01-16 07:50:13 +0500

    improved layouts for phones

--- web-app/app/routes/contacts/index.js
+++ web-app/app/routes/contacts/index.js
@@ -3,6 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   setupController(controller, model) {
-    controller.set('isIndex', true);
+
+    this._super(controller, model);
+
+    // set isIndex flag on parent controller to true
+    // this will make contact list full width for mobile devices
+    this.controllerFor('contacts').set('isIndex', true);
+  },
+
+  deactivate() {
+    this.controllerFor('contacts').set('isIndex', false);
   }
 });
