9c60532fc3e942e907c117cedae11ae17401e80f
Uzair Sajid <uzee@uzeeinc.net>
2017-01-17 08:49:49 +0500

    got a file picker working

--- web-app/app/components/profile-picture.js
+++ web-app/app/components/profile-picture.js
@@ -4,15 +4,33 @@ export default Ember.Component.extend({
 
   classNames: ['thumbnail'],
 
+  uploadTitle: "Step 1: Select a Picture",
+  uploadLabel: "Upload Picture",
+  allowUpload: Ember.computed.not('model.hasAvatar'),
+
   actions: {
 
     removePicture(model) {
 
       model.set('avatarUrl', null);
 
-      model.save().then(response => {
+      model.save().then(() => {
         this.set('confirmRemove', false);
       });
     },
+
+    cancelUpload() {
+      this.set('uploadModal', false);
+    },
+
+    handleUpload() { console.log('Handle upload...'); },
+
+    // file-picker actions
+    fileLoaded(file) {
+
+      this.set('uploadTitle', 'Step 2: Crop your Picture');
+
+      console.log(file);
+    }
   }
 });
